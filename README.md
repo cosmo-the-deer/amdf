421738269f787a70e9b956269030d2afc4d95f0a
cosmo-the-deer <cosmothedeer@gmail.com>
2025-07-20 21:39:32 +1200

    broken code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # amdf
 adavanced meta data file
+
+----
+* line 1: is hidden (0 or 1)
+* tags
---
+++ amdf.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+
+def get_files_with_tags(tags, folder_path):
+
+    result = [] # A varable in which a list of all file paths are stored.
+
+    # Throw an error if the directory path doesn't exist.
+    if not os.path.isdir(folder_path):
+        raise FileNotFoundError("HEY DUMBASS, THERE IS NO DIRECTORY") # Change error message.
+
+
+    # Get a list of all the files in the folder (not recursive).
+    folder = Path(folder_path)
+    file_paths = [str(f) for f in folder.iterdir() if f.is_file()]
+
+    # Read each file and add them to a list.
+    for file in file_paths:
+        if file.endswith(".amdf"):
+            with open(file) as f:
+                f_lines = f.readlines()
+                f_tags = f_lines[1].strip().split(",")
+                print(f_tags)
+                if any([tag in f_tags for tag in tags]):
+                    result.append(file)
+    return result
+
+    
+print(get_files_with_tags(["furry"], "folder/"))

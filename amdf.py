13d444c58f0a0bb56929ce6dec1efc52a17febc8
cosmo-the-deer <cosmothedeer@gmail.com>
2025-07-21 11:53:50 +1200

    forgot what i did lol :p

--- amdf.py
+++ amdf.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 
 def get_files_with_tags(tags, folder_path):
@@ -25,5 +26,31 @@ def get_files_with_tags(tags, folder_path):
                     result.append(true_file)
     return result
 
+def get_files_with_authors(authors, folder_path):
+
+    result = [] # A varable in which a list of all file paths are stored.
+
+    # Throw an error if the directory path doesn't exist.
+    if not os.path.isdir(folder_path):
+        raise FileNotFoundError("HEY DUMBASS, THERE IS NO DIRECTORY") # Change error message.
+
+
+    # Get a list of all the files in the folder (not recursive).
+    folder = Path(folder_path)
+    file_paths = [str(f) for f in folder.iterdir() if f.is_file()]
+
+    # Read each file and add them to a list.
+    for file in file_paths:
+        if file.endswith(".amdf"):
+            with open(file) as f:
+                f_lines = f.readlines()
+                f_authors = [t.strip().lower() for t in f_lines[2].strip().split(',')]
+                if any([tag in f_authors for tag in authors]):
+                    true_file = file.replace(".amdf", "")
+                    result.append(true_file)
+    return result
+
     
-print(get_files_with_tags(["furry"], "folder/"))
+files = get_files_with_tags(["furry"], "folder/")
+for i in range(len(files)):
+    os.system("open " + str(files[i]))
